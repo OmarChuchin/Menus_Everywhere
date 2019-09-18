bc0e5791cbbebf7107149003f2bf07d72f9a1057
Omar Cuenca <A01378844@itesm.mx>
2019-09-18 18:39:56 -0500

    Merge branch 'master' of /media/omar/DATA/Escuela/Desarrollo_de_apps/Menus_Everywhere with conflicts.

---
+++ app/src/main/java/com/appcoders/menus_everywhere/QRScanner.kt
@@ -0,0 +1,88 @@
+package com.appcoders.menus_everywhere
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_qrscanner.*
+
+class QRScanner : AppCompatActivity() {
+
+    private val IMAGE_CAPTURE_CODE = 200
+    private val PERMISION_CODE = 100
+    private var image_rui: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrscanner)
+
+        btnActivateCamera.setOnClickListener { view ->
+            this.askForPermissions()
+        }
+
+    }
+
+    private fun askForPermissions() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(android.Manifest.permission.CAMERA)==PackageManager.PERMISSION_DENIED || checkSelfPermission(android.Manifest.permission.INTERNET)==PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_DENIED || checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED){
+                //there are no permissions for the app to work properly
+                val permissions = arrayOf(Manifest.permission.CAMERA,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permissions,this.PERMISION_CODE)
+            }
+            else {
+                //There are permissions for the app
+                this.openCamera()
+            }
+        }
+        else{
+            //There is no need for permissions apparently
+            this.openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Taken just now")
+        this.image_rui = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, this.image_rui)
+        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISION_CODE -> {
+                if(grantResults.size>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    //Permission from popup was granted
+                }
+                else{
+                    Toast.makeText(this,"You need to grant access for the app to work",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            imgPhoto.setImageURI(this.image_rui)
+        }
+    }
+
+
+}
