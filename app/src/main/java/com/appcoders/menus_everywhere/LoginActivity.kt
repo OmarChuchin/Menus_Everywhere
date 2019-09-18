977064683fda3a91d01a8a812160e877f668f65b
Jesus Perea <jesus.perea@outlook.com>
2019-09-18 17:44:11 -0500

    Manifest and login button

--- app/src/main/java/com/appcoders/menus_everywhere/LoginActivity.kt
+++ app/src/main/java/com/appcoders/menus_everywhere/LoginActivity.kt
@@ -2,12 +2,15 @@ package com.appcoders.menus_everywhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginBtn.setOnClickListener { view ->
 
+        }
     }
 }
