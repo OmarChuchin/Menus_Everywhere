b58852b0829bc987d9e40a14201c69b0c1c25c86
Jesus Perea <jesus.perea@outlook.com>
2019-09-18 18:16:52 -0500

    Register laayout

--- app/src/main/java/com/appcoders/menus_everywhere/LoginActivity.kt
+++ app/src/main/java/com/appcoders/menus_everywhere/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.appcoders.menus_everywhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
@@ -10,7 +11,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         loginBtn.setOnClickListener { view ->
-
+            val intMain = Intent(this, MainActivity::class.java);
+            startActivity(intMain);
+            finish();
+        }
+        registerButton.setOnClickListener { view ->
+            val intRegister = Intent(this, RegisterActivity::class.java);
+            startActivity(intRegister);
         }
     }
 }
