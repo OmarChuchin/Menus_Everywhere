48a0864f9e8842cb458ac7f6e625853b13dc440e
val8al <val8al@gmail.com>
2019-09-19 09:28:42 -0500

    first commit: settings

--- app/src/main/java/com/appcoders/menus_everywhere/SettingsActivity.kt
+++ app/src/main/java/com/appcoders/menus_everywhere/SettingsActivity.kt
@@ -1,12 +1,22 @@
 package com.appcoders.menus_everywhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        settingsLogoutBtn.setOnClickListener { view ->
+            val loginActivity = Intent(this, LoginActivity::class.java);
+            startActivity(loginActivity);
+
+        }
+
     }
+
 }
