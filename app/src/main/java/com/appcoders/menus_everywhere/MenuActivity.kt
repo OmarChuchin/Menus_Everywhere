706b8e759ed937c5b3dd63e0972139093252158c
Jesus Perea <jesus.perea@outlook.com>
2019-09-19 13:36:47 -0500

    Menu y productos

---
+++ app/src/main/java/com/appcoders/menus_everywhere/MenuActivity.kt
@@ -0,0 +1,19 @@
+package com.appcoders.menus_everywhere
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        buttonSencilla.setOnClickListener { view ->
+            val intProduct = Intent(this, ProductActivity::class.java);
+            startActivity(intProduct)
+        }
+
+    }
+}
