80bdd12e9a860eec65672ab184889cf75e2426eb
Jesus Perea <jesus.perea@outlook.com>
2019-10-09 20:08:22 -0500

    Log out button

--- app/src/main/java/com/appcoders/menus_everywhere/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/appcoders/menus_everywhere/ui/settings/SettingsFragment.kt
@@ -1,16 +1,18 @@
 package com.appcoders.menus_everywhere.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.appcoders.menus_everywhere.LoginActivity
 import com.appcoders.menus_everywhere.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_setting.view.*
 
 class SettingsFragment : Fragment() {
-
+    var fbAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,9 +23,16 @@ class SettingsFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_setting, container, false)
         val button = root.logutButton;
         button.setOnClickListener { view ->
-            FirebaseAuth.getInstance().signOut()
+            signOut()
+            val int = Intent(inflater.context, LoginActivity::class.java)
+            startActivity(int)
         }
 
         return root
+    }
+
+    fun signOut(){
+        fbAuth.signOut()
+
     }
 }
