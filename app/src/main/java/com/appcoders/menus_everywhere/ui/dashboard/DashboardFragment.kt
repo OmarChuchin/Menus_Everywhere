b50cc63a4750184b0e3cfddbd4e6b07db4657839
Jesus Perea <jesus.perea@outlook.com>
2019-10-09 19:25:51 -0500

    QR Implementation

--- app/src/main/java/com/appcoders/menus_everywhere/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/appcoders/menus_everywhere/ui/dashboard/DashboardFragment.kt
@@ -12,20 +12,15 @@ import com.appcoders.menus_everywhere.R
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        textView.text = "Hola Scan";
         return root
     }
 }
