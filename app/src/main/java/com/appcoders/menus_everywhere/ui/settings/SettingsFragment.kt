78f2bea0c97c08781f485f7dfbf510369834670a
Jesus Perea <jesus.perea@outlook.com>
2019-10-09 19:20:51 -0500

    QR Implementation

--- app/src/main/java/com/appcoders/menus_everywhere/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/appcoders/menus_everywhere/ui/settings/SettingsFragment.kt
@@ -1,18 +1,15 @@
-package com.appcoders.menus_everywhere.ui.notifications
+package com.appcoders.menus_everywhere.ui.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.appcoders.menus_everywhere.R
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.fragment_notifications.view.*
+import kotlinx.android.synthetic.main.fragment_setting.view.*
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
 
     override fun onCreateView(
@@ -21,7 +18,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val root = inflater.inflate(R.layout.fragment_setting, container, false)
         val button = root.logutButton;
         button.setOnClickListener { view ->
             FirebaseAuth.getInstance().signOut()
