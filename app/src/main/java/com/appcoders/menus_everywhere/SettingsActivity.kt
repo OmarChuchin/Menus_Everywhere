9a4e263d8737e831fbd4b48642da86ba19fa8bc8
Jesus Perea <jesus.perea@outlook.com>
2019-10-13 19:01:30 -0500

    Log out button

--- app/src/main/java/com/appcoders/menus_everywhere/SettingsActivity.kt
+++ app/src/main/java/com/appcoders/menus_everywhere/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.appcoders.menus_everywhere
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         settingsLogoutBtn.setOnClickListener { view ->
+            FirebaseAuth.getInstance().signOut();
             val loginActivity = Intent(this, LoginActivity::class.java);
             startActivity(loginActivity);
 
